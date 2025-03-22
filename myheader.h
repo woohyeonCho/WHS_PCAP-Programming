e6f4f1c08adf33b7a20b0fd657cb2e381bbf1e27
woohyeonCho <109808283+woohyeonCho@users.noreply.github.com>
2025-03-22 18:25:06 +0900

    최종 myheader
    .

--- myheader.h
+++ myheader.h
@@ -1,75 +1,40 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
 
-/* Ethernet header */
+#include <pcap.h>
+
+// Ethernet 헤더 (14바이트)
 struct ethheader {
-    u_char  ether_dhost[6];    /* destination host address */
-    u_char  ether_shost[6];    /* source host address */
-    u_short ether_type;                     /* IP? ARP? RARP? etc */
+    u_char dst_mac[6];
+    u_char src_mac[6];
+    u_short type;
 };
 
-/* IP Header */
+// IP 헤더
 struct ipheader {
-  unsigned char      iph_ihl:4, //IP header length
-                     iph_ver:4; //IP version
-  unsigned char      iph_tos; //Type of service
-  unsigned short int iph_len; //IP Packet length (data + header)
-  unsigned short int iph_ident; //Identification
-  unsigned short int iph_flag:3, //Fragmentation flags
-                     iph_offset:13; //Flags offset
-  unsigned char      iph_ttl; //Time to Live
-  unsigned char      iph_protocol; //Protocol type
-  unsigned short int iph_chksum; //IP datagram checksum
-  struct  in_addr    iph_sourceip; //Source IP address
-  struct  in_addr    iph_destip;   //Destination IP address
+    u_char  iph_verlen;        // Version (4 bits) + Internet header length (4 bits)
+    u_char  iph_tos;           // Type of service
+    u_short iph_len;           // Total length
+    u_short iph_ident;         // Identification
+    u_short iph_flags;         // Flags + Fragment offset
+    u_char  iph_ttl;           // Time to live
+    u_char  iph_protocol;      // Protocol
+    u_short iph_chksum;        // Header checksum
+    struct  in_addr iph_src;   // Source IP
+    struct  in_addr iph_dest;  // Destination IP
 };
 
-/* ICMP Header  */
-struct icmpheader {
-  unsigned char icmp_type; // ICMP message type
-  unsigned char icmp_code; // Error code
-  unsigned short int icmp_chksum; //Checksum for ICMP Header and data
-  unsigned short int icmp_id;     //Used for identifying request
-  unsigned short int icmp_seq;    //Sequence number
-};
-
-/* UDP Header */
-struct udpheader
-{
-  u_int16_t udp_sport;           /* source port */
-  u_int16_t udp_dport;           /* destination port */
-  u_int16_t udp_ulen;            /* udp length */
-  u_int16_t udp_sum;             /* udp checksum */
-};
-
-/* TCP Header */
+// TCP 헤더
 struct tcpheader {
-    u_short tcp_sport;               /* source port */
-    u_short tcp_dport;               /* destination port */
-    u_int   tcp_seq;                 /* sequence number */
-    u_int   tcp_ack;                 /* acknowledgement number */
-    u_char  tcp_offx2;               /* data offset, rsvd */
-#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
-    u_char  tcp_flags;
-#define TH_FIN  0x01
-#define TH_SYN  0x02
-#define TH_RST  0x04
-#define TH_PUSH 0x08
-#define TH_ACK  0x10
-#define TH_URG  0x20
-#define TH_ECE  0x40
-#define TH_CWR  0x80
-#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-    u_short tcp_win;                 /* window */
-    u_short tcp_sum;                 /* checksum */
-    u_short tcp_urp;                 /* urgent pointer */
+    u_short th_sport;          // Source port
+    u_short th_dport;          // Destination port
+    u_int   th_seq;            // Sequence number
+    u_int   th_ack;            // Acknowledgement number
+    u_char  th_offx2;          // Data offset, reserved
+    u_char  th_flags;
+    u_short th_win;            // Window
+    u_short th_sum;            // Checksum
+    u_short th_urp;            // Urgent pointer
 };
 
-/* Psuedo TCP header */
-struct pseudo_tcp
-{
-        unsigned saddr, daddr;
-        unsigned char mbz;
-        unsigned char ptcl;
-        unsigned short tcpl;
-        struct tcpheader tcp;
-        char payload[1500];
-};
+#endif
